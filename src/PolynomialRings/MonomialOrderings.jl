5299c28f41957af68be21adbbf7f7220ab8e0b6a
Timo Kluck <tkluck@infty.nl>
2017-06-24 10:38:52 +0200

    For unbounded number of generators, take deglex
    I don't definitely see why degrevlex wouldn't be a proper
    monomial order, but I also haven't proved that.
    
    Also, tiny fixup for the case where we do use degrevlex.

--- src/PolynomialRings/MonomialOrderings.jl
+++ src/PolynomialRings/MonomialOrderings.jl
@@ -4,13 +4,28 @@ import Base: isless
 import PolynomialRings.Monomials: AbstractMonomial, total_degree, num_variables
 
 function isless(a::M,b::M,::Type{Val{:degrevlex}}) where M <: AbstractMonomial
+
     if total_degree(a) == total_degree(b)
-        i = num_variables(a)
+        i = max(num_variables(a), num_variables(b))
         while i >= 1
             if a[i] != b[i]
                 return a[i] > b[i]
             end
             i -= 1
+        end
+        return false
+    else
+        return total_degree(a) < total_degree(b)
+    end
+end
+
+function isless(a::M,b::M,::Type{Val{:deglex}}) where M <: AbstractMonomial
+
+    if total_degree(a) == total_degree(b)
+        for i in 1:max(num_variables(a), num_variables(b))
+            if a[i] != b[i]
+                return a[i] < b[i]
+            end
         end
         return false
     else
