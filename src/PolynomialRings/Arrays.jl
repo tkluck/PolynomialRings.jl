0a6aea661283e65609ed28f8ec22aaa8d29e1bd3
Timo Kluck <tkluck@infty.nl>
2017-07-05 17:48:37 +0200

    Add det(...) and *-broadcasting for polynomial matrices
    The generic implementation of det(...) seems to assume the coefficients
    are in a field, which is why I'm adding an 'even more generic' version
    here. It's the slowest possible implementation and should probably be
    replaced by some version of row and column operations.
    
    As for *-broadcasting, this is not happening because Polynomial is not
    a Number.

--- src/PolynomialRings/Arrays.jl
+++ src/PolynomialRings/Arrays.jl
@@ -1,11 +1,47 @@
 module Arrays
 
 import PolynomialRings: to_dense_monomials, max_variable_index, to_dense_monomials
+import PolynomialRings.Polynomials: Polynomial
 import PolynomialRings.NamedPolynomials: NamedPolynomial
+
+_P = Union{Polynomial, NamedPolynomial}
+
+
+# -----------------------------------------------------------------------------
+#
+# Imports for overloading
+#
+# -----------------------------------------------------------------------------
+import Base: *, det
+
 
 function to_dense_monomials(a::AbstractArray{NP}) where NP <: NamedPolynomial
     n = maximum(max_variable_index(a_i) for a_i in a)
     [ to_dense_monomials(n, a_i) for a_i in a]
 end
+
+function det(m::M) where M <: AbstractMatrix{P} where P <: _P
+    n,k = size(m)
+    n == k || throw(ArgumentError("Cannot compute determinant of an $n x $k matrix"))
+
+    if n == 1
+        return m[1,1]
+    end
+
+    return (
+        sum(
+            m[i,1] * det([ m[1:(i-1),2:n] ; m[(i+1):n,2:n] ])
+            for i=1:2:n
+        )
+        -
+        sum(
+            m[i,1] * det([ m[1:(i-1),2:n] ; m[(i+1):n,2:n] ])
+            for i=2:2:n
+        )
+    )
+end
+
+*(A::_P, B::AbstractArray) = broadcast(*, A, B)
+*(A::AbstractArray, B::_P) = broadcast(*, A, B)
 
 end
