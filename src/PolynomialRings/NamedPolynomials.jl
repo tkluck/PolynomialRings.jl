9b7e7e82de32a0e33a56eb16d902ced279dd98cc
Timo Kluck <tkluck@infty.nl>
2017-07-08 17:32:36 +0200

    Add some documentation

--- src/PolynomialRings/NamedPolynomials.jl
+++ src/PolynomialRings/NamedPolynomials.jl
@@ -87,7 +87,22 @@ lcm_multipliers(a::NP, b::NP) where NP <: NamedPolynomial = ((m_a,m_b) = lcm_mul
 # Constructing polynomial_rings
 #
 # -----------------------------------------------------------------------------
+"""
+    polynomial_ring(basering::Type, symbols::Symbol...)
+
+Create a type for the polynomial ring over `basering` in variables with names
+specified by `symbols`, and return the type and a tuple of these variables.
+
+# Examples
+```jldoctest
+julia> R,(x,y,z) = polynomial_ring(Int, :x, :y, :z)
+julia> x*y + z
+1 z + 1 x y
+```
+"""
 function polynomial_ring(basering::Type, symbols::Symbol...)
+    length(symbols)>0 || throw(ArgumentError("Need at least one variable name"))
+
     Names = Tuple{symbols...}
     P = Polynomial{Vector{Term{TupleMonomial{length(symbols),Int}, basering}}, :degrevlex}
     gens = generators(P)
@@ -96,6 +111,28 @@ function polynomial_ring(basering::Type, symbols::Symbol...)
     return NP, map(g->NP(g), gens)
 end
 
+"""
+    formal_coefficients(R, name::Symbol)
+
+Return a `Channel` with formal coefficients for the polynomial ring `R`.
+
+Formal coefficients means that these are generators for a polynomial ring
+`C` with an unbounded number of variables, and this polynomial ring is used
+(through base extension) as the coefficients for `R`.
+
+In other words, the channel yields `c_i⊗ 1` for generators `c_i ∈ C` and `1 ∈ R`.
+
+# Examples
+```jldoctest
+julia> R,(x,) = polynomial_ring(Int, :x);
+julia> coeffs = formal_coefficients(R, :c);
+julia> c() = take!(coeffs);
+julia> [c()*x^2 + c()*x + c() , c()*x^2 + c()*x + c()]
+2-element Array{PolynomialRings.NamedPolynomials.NamedPolynomial{PolynomialRings.Polynomials.Polynomial{Array{PolynomialRings.Terms.Term{PolynomialRings.Monomials.TupleMonomial{1,Int64},PolynomialRings.NamedPolynomials.NamedPolynomial{PolynomialRings.Polynomials.Polynomial{Array{PolynomialRings.Terms.Term{PolynomialRings.Monomials.VectorMonomial{SparseVector{Int64,Int64}},Int64},1},:deglex},:c}},1},:degrevlex},Tuple{:x}},1}:
+ 1 c3 + 1 c2 x + 1 c1 x^2
+ 1 c6 + 1 c5 x + 1 c4 x^2
+```
+"""
 function formal_coefficients(::Type{NP}, name::Symbol) where NP <: NamedPolynomial
     C = Polynomial{Vector{Term{VectorMonomial{SparseVector{Int,Int}}, Int}}, :deglex}
     CC = NamedPolynomial{C, name}
