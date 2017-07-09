c8513548a9b052e9c9ad84abb137677ac9ffb087
Timo Kluck <tkluck@infty.nl>
2017-07-09 11:31:29 +0200

    Add scalar multiplication for terms

--- src/PolynomialRings/Terms.jl
+++ src/PolynomialRings/Terms.jl
@@ -39,6 +39,8 @@ exptype(::Type{T}) where T<:Term = exptype(monomialtype(T))
 #
 # -----------------------------------------------------------------------------
 *(a::Term{M, C1}, b::Term{M, C2}) where M <: AbstractMonomial where {C1,C2} = Term(a.m*b.m, a.c*b.c)
+*(a::Term{M, C}, b::C) where M <: AbstractMonomial where C = Term(a.m, a.c*b)
+*(a::C, b::Term{M, C}) where M <: AbstractMonomial where C = Term(b.m, a*b.c)
 +(a::T)            where T <: Term = a
 -(a::T)            where T <: Term = T(a.m, -a.c)
 ==(a::T,b::T)      where T <: Term = a.m == b.m && a.c == b.c
