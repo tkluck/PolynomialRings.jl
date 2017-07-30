578203fef8773415f9677f81bf5652fabf35eee5
Timo Kluck <tkluck@infty.nl>
2017-07-30 15:03:34 +0200

    Methods.jl: use `function <name> end` for body-less functions to be overridden
    I didn't really know about this syntax when I created the
    throw(AssertionError("not implemented")) versions of these.

--- src/PolynomialRings/Methods.jl
+++ src/PolynomialRings/Methods.jl
@@ -1,41 +1,33 @@
-generators(x)           = throw(AssertionError("Not implemented: generators(::$(typeof(x)))"))
-⊗(x)                    = throw(AssertionError("Not implemented: ⊗(::$(typeof(x)))"))
-to_dense_monomials(n,x) = throw(AssertionError("Not implemented: to_dense_monomials(::$(typeof(n)), ::$(typeof(x)))"))
-max_variable_index(x)   = throw(AssertionError("Not implemented: max_variable_index(::$(typeof(x)))"))
-
-base_extend(::Type{A}, ::Type{B}) where {A,B} = promote_type(A,B)
-
-export generators, ⊗, to_dense_monomials, max_variable_index, base_extend
+function ⊗ end
+function to_dense_monomials end
+function max_variable_index end
 
 # -----------------------------------------------------------------------------
 #
 # Type information
 #
 # -----------------------------------------------------------------------------
+generators(x::Type)    = throw(AssertionError("Not implemented: generators(::Type{$x})"))
 basering(x::Type)      = throw(AssertionError("Not implemented: basering(::Type{$x})"))
 monomialtype(x::Type)  = throw(AssertionError("Not implemented: monomialtype(::Type{$x})"))
 monomialorder(x::Type) = throw(AssertionError("Not implemented: monomialorder(::Type{$x})"))
 termtype(x::Type)      = throw(AssertionError("Not implemented: termtype(::Type{$x})"))
 exptype(x::Type)       = throw(AssertionError("Not implemented: exptype(::Type{$x})"))
 
+generators(x)    = generators(typeof(x))
 basering(x)      = basering(typeof(x))
 monomialtype(x)  = monomialtype(typeof(x))
 monomialorder(x) = monomialorder(typeof(x))
 termtype(x)      = termtype(typeof(x))
 exptype(x)       = exptype(typeof(x))
 
+base_extend(::Type{A}, ::Type{B}) where {A,B} = promote_type(A,B)
+
 # -----------------------------------------------------------------------------
 #
 # Polynomial/term/monomial operations
 #
 # -----------------------------------------------------------------------------
-deg(x)               = throw(AssertionError("Not implemented: deg(::$(typeof(x)))"))
-terms(x)             = throw(AssertionError("Not implemented: terms(::$(typeof(x)))"))
-leading_term(x)      = throw(AssertionError("Not implemented: leading_term(::$(typeof(x)))"))
-maybe_div(a,b)       = throw(AssertionError("Not implemented: maybe_div(::$(typeof(a)), ::$(typeof(b)))"))
-lcm_multipliers(a,b) = throw(AssertionError("Not implemented: lcm_multipliers(::$(typeof(a)), ::$(typeof(b)))"))
-lcm_degree(a,b)      = throw(AssertionError("Not implemented: lcm_degree(::$(typeof(a)), ::$(typeof(b)))"))
-
 """
     deg(f)
 
@@ -55,3 +47,10 @@ julia> deg(@coefficient(c1*x^2, x^2))
 ```
 """
 function deg end
+function terms end
+function leading_term end
+function maybe_div end
+function lcm_multipliers end
+function lcm_degree end
+
+export generators, ⊗, to_dense_monomials, max_variable_index, base_extend
