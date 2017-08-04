8bb847c17b4637a630386ba6be61e865ac9c5482
Timo Kluck <tkluck@infty.nl>
2017-08-05 01:02:29 +0200

    Simplify + unify monomial functions

--- src/PolynomialRings/MonomialOrderings.jl
+++ src/PolynomialRings/MonomialOrderings.jl
@@ -2,19 +2,17 @@ module MonomialOrderings
 
 import Base.Order: Ordering, lt
 
-import PolynomialRings.Monomials: AbstractMonomial, VectorMonomial, total_degree, num_variables
+import PolynomialRings.Monomials: AbstractMonomial, VectorMonomial, total_degree, index_union, rev_index_union
 
 type MonomialOrder{Name} <: Ordering end
 
 function lt(::MonomialOrder{:degrevlex}, a::M,b::M) where M <: AbstractMonomial
 
     if total_degree(a) == total_degree(b)
-        i = max(num_variables(a), num_variables(b))
-        while i >= 1
+        for i in rev_index_union(a,b)
             if a[i] != b[i]
                 return a[i] > b[i]
             end
-            i -= 1
         end
         return false
     else
@@ -25,7 +23,7 @@ end
 function lt(::MonomialOrder{:deglex}, a::M,b::M) where M <: AbstractMonomial
 
     if total_degree(a) == total_degree(b)
-        for i in 1:max(num_variables(a), num_variables(b))
+        for i in index_union(a,b)
             if a[i] != b[i]
                 return a[i] < b[i]
             end
@@ -35,48 +33,5 @@ function lt(::MonomialOrder{:deglex}, a::M,b::M) where M <: AbstractMonomial
         return total_degree(a) < total_degree(b)
     end
 end
-
-function lt(::MonomialOrder{:degrevlex}, a::VectorMonomial{V},b::VectorMonomial{V}) where V <: AbstractSparseVector
-    if total_degree(a) == total_degree(b)
-        ia = findlast(a.e)
-        ib = findlast(b.e)
-        while ia != 0 && ib != 0
-            if ia != ib
-                return ia > ib
-            end
-            i = ia
-            if a.e[i] != b.e[i]
-                return a.e[i] > b.e[i]
-            end
-            ia = findprev(a.e,i-1)
-            ib = findprev(b.e,i-1)
-        end
-        return ib == 0 && ia != 0
-    else
-        return total_degree(a) < total_degree(b)
-    end
-end
-
-function lt(::MonomialOrder{:deglex},a::VectorMonomial{V},b::VectorMonomial{V}) where V <: AbstractSparseVector
-    if total_degree(a) == total_degree(b)
-        ia = findfirst(a.e)
-        ib = findfirst(b.e)
-        while ia !=0 && ib != 0
-            if ia != ib
-                return ia > ib
-            end
-            i = ia
-            if a.e[i] != b.e[i]
-                return a.e[i] < b.e[i]
-            end
-            ia = findnext(a.e,i+1)
-            ib = findnext(b.e,i+1)
-        end
-        return ia == 0 && ib != 0
-    else
-        return total_degree(a) < total_degree(b)
-    end
-end
-
 
 end
--- src/PolynomialRings/Monomials.jl
+++ src/PolynomialRings/Monomials.jl
@@ -16,8 +16,8 @@ by a number (e.g. the index in the array/tuple).
 
 Each concrete implementation should implement:
     m[i]
-    num_variables(m)
-    _construct(M, i -> exponent, num_variables)
+    nzindices(m)
+    _construct(M, i -> exponent, nonzero_indices, [total_degree])
     exptype(M)
 
 and optionally:
@@ -40,42 +40,104 @@ import Base: getindex, gcd, lcm, one, *, ^, enumerate, ==, diff
 import PolynomialRings: generators, to_dense_monomials, max_variable_index, deg
 import PolynomialRings: maybe_div, lcm_multipliers, exptype, lcm_degree
 
+# -----------------------------------------------------------------------------
+#
+# Utility: iterate over the union of two index sets
+#
+# -----------------------------------------------------------------------------
+import Base: start, done, next, last, findlast
+type IndexUnion{I,J,lt}
+    left::I
+    right::J
+end
+
+start(i::IndexUnion) = (start(i.left), start(i.right))
+function next(i::IndexUnion{I,J,lt}, state) where {I,J,lt}
+    lstate, rstate = state
+    ldone = done(i.left, lstate)
+    rdone = done(i.right, rstate)
+
+    if !ldone
+        (litem, lnextstate) = next(i.left, lstate)
+    end
+    if !rdone
+        (ritem, rnextstate) = next(i.right, rstate)
+    end
+    if rdone || (!ldone && lt(litem, ritem))
+        return (litem, (lnextstate, rstate))
+    elseif ldone || (!rdone && lt(ritem, litem))
+        return (ritem, (lstate, rnextstate))
+    elseif litem == ritem
+        return (litem, (lnextstate, rnextstate))
+    else
+        @assert(false) # unreachable?
+    end
+end
+done(i::IndexUnion, state) = done(i.left, state[1]) && done(i.right, state[2])
+
+findlast(i::IndexUnion) = max(findlast(i.left), findlast(i.right))
+
+function last(i::IndexUnion)
+    l = findlast(i.left)
+    r = findlast(i.right)
+    if l == 0
+        return i.right[r]
+    elseif r == 0
+        return i.left[l]
+    else
+        return max(i.left[l], i.right[r])
+    end
+end
+
+function index_union(a::AbstractMonomial, b::AbstractMonomial)
+    l = nzindices(a)
+    r = nzindices(b)
+    IndexUnion{typeof(l),typeof(r),<}(l,r)
+end
+
+function rev_index_union(a::AbstractMonomial, b::AbstractMonomial)
+    l = reverse(nzindices(a))
+    r = reverse(nzindices(b))
+    IndexUnion{typeof(l),typeof(r),>}(l,r)
+end
 
 # -----------------------------------------------------------------------------
 #
 # Abstract fallbacks
 #
 # -----------------------------------------------------------------------------
+@inline _construct(::Type{M}, f, nzindices, deg) where M <: AbstractMonomial = _construct(M, f, nzindices)
+@inline _construct(::Type{M}, f, nzindices) where M <: AbstractMonomial = _construct(M, f, nzindices, mapreduce(f, +, zero(exptype(M)), nzindices))
 
-*(a::M, b::M) where M <: AbstractMonomial = _construct(M,i -> a[i] + b[i], max(num_variables(a), num_variables(b)))
-^(a::M, n::Integer) where M <: AbstractMonomial = _construct(M,i -> a[i]*n, num_variables(a))
+one(::Type{M}) where M <: AbstractMonomial = _construct(M, i->0, 1:0)
 
-total_degree(a::A) where A <: AbstractMonomial = sum( a[i] for i in 1:num_variables(a) )
+*(a::M, b::M) where M <: AbstractMonomial = _construct(M,i -> a[i] + b[i], index_union(a,b), total_degree(a) + total_degree(b))
+^(a::M, n::Integer) where M <: AbstractMonomial = _construct(M,i -> a[i]*n, nzindices(a), total_degree(a)*n)
 
-lcm(a::M, b::M) where M <: AbstractMonomial = _construct(M,i -> max(a[i], b[i]), max(num_variables(a), num_variables(b)))
-gcd(a::M, b::M) where M <: AbstractMonomial = _construct(M,i -> min(a[i], b[i]), max(num_variables(a), num_variables(b)))
+total_degree(a::A) where A <: AbstractMonomial = (ix = nzindices(a); length(ix)==0 ? zero(exptype(a)) : sum( a[i] for i in nzindices(a) ) )
+
+lcm(a::M, b::M) where M <: AbstractMonomial = _construct(M,i -> max(a[i], b[i]), index_union(a,b))
+gcd(a::M, b::M) where M <: AbstractMonomial = _construct(M,i -> min(a[i], b[i]), index_union(a,b))
 enumerate(a::M) where M <: AbstractMonomial = Channel(ctype=Tuple{Int,exptype(M)}) do ch
-    for i = 1:num_variables(a)
+    for i in nzindices(a)
         push!(ch, (i, a[i]))
     end
 end
 
 exptype(a::AbstractMonomial) = exptype(typeof(a))
-num_variables(a::A) where A <: AbstractMonomial = num_variables(A)
 
 function maybe_div(a::M, b::M) where M <: AbstractMonomial
-    if all(a[i] >= b[i] for i=1:max(num_variables(a), num_variables(b)))
-        return _construct(M,i -> a[i] - b[i], max(num_variables(a), num_variables(b)))
+    if all(a[i] >= b[i] for i in index_union(a,b))
+        return _construct(M,i -> a[i] - b[i], index_union(a,b))
     else
         return null
     end
 end
 
 function lcm_multipliers(a::M, b::M)::Tuple{M,M} where M <: AbstractMonomial
-    N = max(num_variables(a), num_variables(b))
     return (
-        _construct(M, i -> max(a[i], b[i]) - a[i], N),
-        _construct(M, i -> max(a[i], b[i]) - b[i], N),
+        _construct(M, i -> max(a[i], b[i]) - a[i], index_union(a,b)),
+        _construct(M, i -> max(a[i], b[i]) - b[i], index_union(a,b)),
     )
 end
 
@@ -84,13 +146,12 @@ function diff(a::M, i::Integer) where M <: AbstractMonomial
     if iszero(n)
         return (n, one(M))
     else
-        return (n, _construct(M, j -> (j==i ? a[j]-one(exptype(M)) : a[j]), num_variables(a)))
+        return (n, _construct(M, j -> (j==i ? a[j]-one(exptype(M)) : a[j]), nzindices(a)))
     end
 end
 
 function lcm_degree(a::M, b::M) where M <: AbstractMonomial
-    N = max(num_variables(a), num_variables(b))
-    return sum(max(a[i],b[i]) for i=1:N)
+    return sum(max(a[i],b[i]) for i in index_union(a,b))
 end
 
 # -----------------------------------------------------------------------------
@@ -111,18 +172,16 @@ struct TupleMonomial{N, I} <: AbstractMonomial
     TupleMonomial{N,I}(e,deg) where I <: Integer where N = new(e,deg)
 end
 
-@generated function _construct(::Type{TupleMonomial{N,I}}, f::Function, num_variables::Type{Val{N}}) where {N,I}
+@generated function _construct(::Type{TupleMonomial{N,I}}, f::Function, nonzero_indices, deg) where {N,I}
     result = :( tuple() )
     for i in 1:N
         push!(result.args, :( I(f($i)) ))
     end
     return quote
         t = $result
-        TupleMonomial{N, eltype(t)}(t, sum(t))
+        TupleMonomial{N, eltype(t)}(t, deg)
     end
 end
-
-_construct(::Type{T}, f::Function, num_variables::Int) where T <: TupleMonomial= _construct(T, f, Val{num_variables})
 
 TupleMonomial(e::NTuple{N,I}) where I <: Integer where N = TupleMonomial{N,I}(e,sum(e))
 
@@ -130,12 +189,13 @@ num_variables(::Type{TupleMonomial{N,I}}) where {N,I} = N
 exptype(::Type{TupleMonomial{N,I}}) where I <: Integer where N = I
 @inline getindex(m::TupleMonomial, i::Integer) = m.e[i]
 
-one(::Type{T}) where T<:TupleMonomial = _construct(T, i->zero(exptype(T)), Val{num_variables(T)})
-
 generators(::Type{TupleMonomial{N, I}}) where {N, I} = [
-    _construct(TupleMonomial{N, I}, i->i==j?one(I):zero(I), Val{N})
+    _construct(TupleMonomial{N, I}, i->i==j?one(I):zero(I), 1:N)
     for j in 1:N
 ]
+
+nzindices(a::TupleMonomial{N,I}) where {N,I} = 1:N
+@inline index_union(::T, ::T) where T<:TupleMonomial{N,I} where {N,I} = 1:N
 
 # -----------------------------------------------------------------------------
 #
@@ -159,20 +219,24 @@ struct VectorMonomial{V} <: AbstractMonomial
     VectorMonomial{V}(e) where V <: AbstractVector{<:Integer} = new(e)
 end
 
-function _construct(::Type{M}, f::Function, num_variables::Int) where M <: VectorMonomial{V} where V <: AbstractVector{I} where I <: Integer
-    e = [f(i) for i in 1:num_variables]
-    M(e)
+function _construct(::Type{M}, f::Function, nonzero_indices) where M <: VectorMonomial{V} where V <: AbstractVector{I} where I <: Integer
+    if findlast(nonzero_indices) == 0
+        return M(V())
+    else
+        e = zeros(I, last(nonzero_indices))
+        for i in nonzero_indices
+            e[i] = f(i)
+        end
+        return M(e)
+    end
 end
 
-num_variables(m::VectorMonomial) = length(m.e)
 exptype(::Type{VectorMonomial{V}}) where V = eltype(V)
 @inline getindex(m::VectorMonomial, i::Integer) = i <= length(m.e) ? m.e[i] : zero(exptype(m))
 
-# the empty vector corresponds to all exponents equal to zero
-one(::Type{VectorMonomial{V}}) where V = VectorMonomial{V}( V() )
 # special case for sparsevectors; for some reason, SparseVector{Int,Int}() does not give
-# an empty vector.
-one(::Type{VectorMonomial{V}}) where V <: SparseVector{A,B} where {A,B} = VectorMonomial{V}( sparsevec(B[],A[]) )
+# an empty vector by default.
+(::Type{V})() where V <: SparseVector{A,B} where {A,B} = sparsevec(B[],A[])
 
 #
 # workaround: for some reason, comparison does't fall through the struct
@@ -188,126 +252,22 @@ generators(::Type{VectorMonomial{V}}) where V = Channel(ctype=VectorMonomial{V})
     throw(AssertionError("typemax exhausted"))
 end
 
+nzindices(a::VectorMonomial) = 1:length(a.e)
+
 # -----------------------------------------------------------------------------
 #
 # TupleMonomial: overloads for speedup
 #
 # -----------------------------------------------------------------------------
-@generated function *(a::M, b::M) where M <: TupleMonomial{N} where N
-    result = :( tuple() )
-    for i in 1:N
-        push!(result.args, :( a[$i] + b[$i] ))
-    end
-    return quote
-        M($result, a.deg + b.deg)
-    end
-end
-
 total_degree(a::TupleMonomial) = a.deg
-
-@generated function _div(a::M, b::M) where M <: TupleMonomial{N} where N
-    result = :( tuple() )
-    for i=1:N
-        push!(result.args, :( a[$i] - b[$i] ))
-    end
-    return quote
-        M($result, a.deg - b.deg)
-    end
-end
-
-function maybe_div(a::M, b::M) where M <: TupleMonomial{N} where N
-    if all(a[i] >= b[i] for i=1:N)
-        return _div(a,b)
-    else
-        return null
-    end
-end
-
 
 # -----------------------------------------------------------------------------
 #
 # VectorMonomial: overloads for speedup
 #
 # -----------------------------------------------------------------------------
-function *(a::M, b::M) where M <: VectorMonomial
-    if length(a.e) >= length(b.e)
-        res = copy(a.e)
-        res[1:length(b.e)] += b.e
-        return M(res)
-    else
-        res = copy(b.e)
-        res[1:length(a.e)] += a.e
-        return M(res)
-    end
-end
-
-total_degree(a::VectorMonomial{V}) where V <: SparseVector = sum(nonzeros(a.e))
-enumerate(a::M) where M <: VectorMonomial{V} where V <: SparseVector = Channel(ctype=Tuple{Int,exptype(M)}) do ch
-    for i in find(a.e)
-        push!(ch, (i, a.e[i]))
-    end
-end
-
-function *(a::M, b::M) where M <: VectorMonomial{V} where V<:SparseVector
-    if length(a.e) >= length(b.e)
-        res = copy(a.e)
-        res[find(b.e)] += nonzeros(b.e)
-        return M(res)
-    else
-        res = copy(b.e)
-        res[find(a.e)] += nonzeros(a.e)
-        return M(res)
-    end
-end
-
-function maybe_div(a::M, b::M) where M <: VectorMonomial{V} where V<:Vector
-    if length(a.e) >= length(b.e)
-        res = copy(a.e)
-        res[find(b.e)] -= nonzeros(b.e)
-        if all(r>=0 for r in res)
-            return M(res)
-        else
-            return null
-        end
-    else
-        res = copy(b.e)
-        res[find(a.e)] -= nonzeros(a.e)
-        if all(r>=0 for r in res)
-            return M(res)
-        else
-            return null
-        end
-    end
-end
-
 import Base.SparseArrays: nonzeroinds
-function maybe_div(a::M, b::M) where M <: VectorMonomial{V} where V<:SparseVector
-    for (ib,exp) in zip(nonzeroinds(b.e), nonzeros(b.e))
-        if a[ib] < exp
-            return null
-        end
-    end
-    res = spzeros(exptype(M), max(num_variables(a), num_variables(b)))
-    ia = findfirst(a.e)
-    ib = findfirst(b.e)
-    while (i = min(ia, ib)) > 0
-        res[i] = a.e[i] - b.e[i]
-
-        ia = findnext(a.e,i+1)
-        ib = findnext(b.e,i+1)
-    end
-    if ia != 0
-        res[ia] = a.e[ia]
-        while (ia = findnext(a.e,ia+1))>0
-            res[ia] = a.e[ia]
-        end
-    end
-    return M(res)
-end
-
-function ^(a::M, n::Integer) where M <: VectorMonomial
-    return M(broadcast(*,a.e,n))
-end
+nzindices(a::VectorMonomial{V}) where V <: SparseVector = nonzeroinds(a.e)
 
 # -----------------------------------------------------------------------------
 #
@@ -318,7 +278,7 @@ end
 max_variable_index(m::TupleMonomial{N}) where N = N
 max_variable_index(m::VectorMonomial{V}) where V = length(m.e)
 
-to_dense_monomials(n::Integer, m::AbstractMonomial) = _construct(TupleMonomial{n,exptype(m)}, i->m[i], n)
+to_dense_monomials(n::Integer, m::AbstractMonomial) = _construct(TupleMonomial{n,exptype(m)}, i->m[i], 1:n)
 
 # -----------------------------------------------------------------------------
 #
