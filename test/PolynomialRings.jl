afadc8166e024dd319bad89ea0dd5b8ee5d9ba74
Timo Kluck <tkluck@infty.nl>
2017-12-30 21:32:48 +0100

    Fix typo in comment

--- test/PolynomialRings.jl
+++ test/PolynomialRings.jl
@@ -122,7 +122,7 @@ one(Foo) = Foo()
         dd1,dd2,dd3 = d[]
         @test to_dense_monomials([dd1, dd2, dd3]) == [d1,d2,d3]
 
-        # the middle one return a tuple, that's why we need to collect()
+        # the middle one returns a tuple, that's why we need to collect()
         # it before comparison
         @test c[1:5] == collect( c[1,2,3,4,5] ) == c[[1,2,3,4,5]]
 
