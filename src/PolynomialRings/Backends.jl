e8146947c18183638805224c04eba468bd907800
Timo Kluck <tkluck@infty.nl>
2017-12-30 18:23:54 +0100

    Add no-arguments version of Backends.set_default(), resetting the default

--- src/PolynomialRings/Backends.jl
+++ src/PolynomialRings/Backends.jl
@@ -3,6 +3,7 @@ module Backends
 module Groebner
     struct Buchberger end
     default = Buchberger()
+    set_default()  = (global default; default=Buchberger())
     set_default(x) = (global default; default=x)
 end
 
