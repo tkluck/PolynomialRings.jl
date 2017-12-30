752aa7fc7d7032ad8236928f8b4430a65bdfe59a
Timo Kluck <tkluck@infty.nl>
2017-12-30 15:19:25 +0100

    Add timing examples with dense polynomials and with lexicographic monomial ordering

--- test/Timings.jl
+++ test/Timings.jl
@@ -14,3 +14,11 @@ cyclic(vars...) = [prod(vars) - 1; [sum(prod, combinations(vars, n)) for n=1:(le
 @ring! ℤ[c[]]
 groebner_basis(cyclic(c[1:3]...)) # compile
 @showtime groebner_basis(cyclic(c[1:9]...))
+
+R, c = polynomial_ring(:c1, :c2, :c3, :c4, :c5, :c6, :c7, :c8, :c9, monomialorder=:degrevlex)
+groebner_basis(cyclic(c[1:3]...)) # compile
+@showtime groebner_basis(cyclic(c[1:9]...))
+
+R, c = polynomial_ring(:c1, :c2, :c3, :c4, :c5, :c6, :c7, :c8, :c9, monomialorder=:lex)
+groebner_basis(cyclic(c[1:3]...)) # compile
+@showtime groebner_basis(cyclic(c[1:9]...))
