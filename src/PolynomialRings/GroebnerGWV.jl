5c7fedfa41d16fd8a1cb4b6f9f68355f2b6ac46d
Timo Kluck <tkluck@infty.nl>
2018-01-01 22:18:51 +0100

    GWV: give closing log messages if we ever did progress updates
    Also, remove a few commented-out info calls.

--- src/PolynomialRings/GroebnerGWV.jl
+++ src/PolynomialRings/GroebnerGWV.jl
@@ -37,7 +37,6 @@ function regular_topreduce_rem(R, m, G)
     i = 1
     supertopreducible = false
     while i <= length(G)
-        #info("considering $(G[i])")
         u2, v2 = G[i]
         if iszero(v2)
             if !iszero(u1) && !iszero(u2) && !isnull(_maybe_div(_leading_monomial(u1), _leading_monomial(u2)))
@@ -46,17 +45,14 @@ function regular_topreduce_rem(R, m, G)
         elseif !iszero(v1)
             t = _maybe_div(_leading_monomial(v1), _leading_monomial(v2))
             if !isnull(t)
-                #info("leading monoms divisible ($t)...")
                 lr1 = _leading_row(u1)
                 lr2 = _leading_row(u2)
                 lm1 = monomial(_leading_term(u1))
                 lm2 = monomial(_leading_term(u2))
                 if lr2 > lr1 || (lr1 == lr2 && Base.Order.lt(monomialorder(R), t * lm2, lm1))
-                    #info("doesn't increase signature...")
                     c = _leading_coefficient(v1) // _leading_coefficient(v2)
                     new_u1 = u1 - c*(t*u2)
                     if _leading_monomial(new_u1) != _leading_monomial(u1)
-                        #info("is super!")
                         supertopreducible = true
                     else
                         u1, v1 = (new_u1, v1 - c*(t*v2))
@@ -101,6 +97,7 @@ function gwv(polynomials::AbstractVector{P}) where P <: Polynomial
     considered = 0
     divisors_considered = 0
     divisor_considerations = 0
+    progress_logged = false
     # step 3
     while length(JP) > 0
         loops += 1
@@ -109,14 +106,13 @@ function gwv(polynomials::AbstractVector{P}) where P <: Polynomial
             k = length(JP)
             h = sum(length, values(H))
             info("GWV: After $loops loops: $l elements in basis; $considered J-pairs considered; |JP|=$k, |H|=$h; $divisor_considerations considerations of divisors ($(divisors_considered/divisor_considerations) divisors on average).")
+            progress_logged = true
         end
 
         # step 1.
         sig, m = first(JP)
         delete!(JP, sig)
         (T, v1) = m
-
-        #info("considering $m")
 
         # step 2: criterion from thm 2.3(c)
         # there is a pair (u_2, v_2) ∈ G so that lm(u_2) divides
@@ -134,9 +130,7 @@ function gwv(polynomials::AbstractVector{P}) where P <: Polynomial
             continue
         end
 
-        #info("Reducing $m")
         (_,v), status = regular_topreduce_rem(R, m, G)
-        #info("Reduction result: $v")
 
         if iszero(v)
             h = _leading_monomial(T)
@@ -186,8 +180,6 @@ function gwv(polynomials::AbstractVector{P}) where P <: Polynomial
                             divisors_considered += 1
                             d in H[Jsig[1]]
                         end
-                            #info("Adding $Jpair for consideration (signature $Jsig)")
-                            #info("(should be reducible either by $((T,v)) or by $((Tj,vj)))")
                             if Jsig in keys(JP)
                                 oldu,oldv = JP[Jsig]
                                 newv = Jpair[2]
@@ -215,10 +207,12 @@ function gwv(polynomials::AbstractVector{P}) where P <: Polynomial
     # --------------------------------------------------------------------------
     result = getindex.(G, 2)
     k = length(result)
+    progress_logged && info("Done; interreducing the $k result polynomials")
     for i in 1:k
         result[i] = rem(result[i], result[[1:i-1; i+1:k]])
     end
     filter!(!iszero, result)
+    progress_logged && info("Done. Returning a Gröbner basis of length $(length(result))")
     # --------------------------------------------------------------------------
     # Return the result
     # --------------------------------------------------------------------------
