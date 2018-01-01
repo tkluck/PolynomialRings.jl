d84e6c964c68093fa663f57779d6fb3ab1bb4253
Timo Kluck <tkluck@infty.nl>
2018-01-01 15:06:01 +0100

    Timings: fix definition of cyclic ideal

--- test/Timings.jl
+++ test/Timings.jl
@@ -9,16 +9,16 @@ macro showtime(expr)
     end
 end
 
-cyclic(vars...) = [prod(vars) - 1; [sum(prod, combinations(vars, n)) for n=1:(length(vars)-1)]]
+cyclic(vars...) = (vars=collect(vars); [prod(vars) - 1; [sum(i->prod(circshift(vars, i)[1:n]), 1:length(vars)) for n=1:(length(vars)-1)]])
 
 @ring! ℤ[c[]]
 groebner_basis(cyclic(c[1:3]...)) # compile
-@showtime groebner_basis(cyclic(c[1:9]...))
+@showtime groebner_basis(cyclic(c[1:5]...))
 
 R, c = polynomial_ring(:c1, :c2, :c3, :c4, :c5, :c6, :c7, :c8, :c9, monomialorder=:degrevlex)
 groebner_basis(cyclic(c[1:3]...)) # compile
-@showtime groebner_basis(cyclic(c[1:9]...))
+@showtime groebner_basis(cyclic(c[1:5]...))
 
 R, c = polynomial_ring(:c1, :c2, :c3, :c4, :c5, :c6, :c7, :c8, :c9, monomialorder=:lex)
 groebner_basis(cyclic(c[1:3]...)) # compile
-@showtime groebner_basis(cyclic(c[1:9]...))
+@showtime groebner_basis(cyclic(c[1:4]...))
