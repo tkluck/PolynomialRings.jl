092ead0d10cb250b64cb7cc5b5211f41ba9bd492
Timo Kluck <tkluck@infty.nl>
2018-01-05 19:27:56 +0100

    Add travis config for auto-building and deploying documentation

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, PolynomialRings
 makedocs(
     modules = [PolynomialRings],
     repo = "https://github.com/tkluck/PolynomialRings.jl.git",
-    doctest = true,
+    doctest = false,
     format = :html,
     sitename = "PolynomialRings.jl",
     authors = "Timo Kluck",
@@ -13,4 +13,11 @@ makedocs(
         "Reference Index" => "reference.md"
         "Types and Functions" => "functions.md"
     ],
+)
+
+deploydocs(
+    deps   = Deps.pip("mkdocs", "python-markdown-math"),
+    repo   = "github.com/tkluck/PolynomialRings.jl.git",
+    julia  = "0.6",
+    osname = "linux",
 )
