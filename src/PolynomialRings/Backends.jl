0017563a8b1c5605a70cbd336919564a5a3b50cb
Timo Kluck <tkluck@infty.nl>
2018-01-24 12:25:59 +0100

    Accept keyword arguments for Gröbner basis functions

--- src/PolynomialRings/Backends.jl
+++ src/PolynomialRings/Backends.jl
@@ -13,12 +13,12 @@ module Gröbner
     import PolynomialRings: gröbner_basis, gröbner_transformation, monomialorder
 
     # fallback in case a backend only provides one, but not the other
-    gröbner_basis(b::Backend, args...)         = gröbner_transformation(b, args...)[1]
-    gröbner_transformation(::Backend, args...) = gröbner_transformation(Buchberger(), args...)
+    gröbner_basis(b::Backend, args...; kwds...)         = gröbner_transformation(b, args...; kwds...)[1]
+    gröbner_transformation(::Backend, args...; kwds...) = gröbner_transformation(Buchberger(), args...; kwds...)
 
     # fallback in case a monomial order is not passed explicitly: choose it from G
-    gröbner_basis(b::Backend, G::AbstractVector, args...) = gröbner_basis(b, monomialorder(eltype(G)), G, args...)
-    gröbner_transformation(b::Backend, G::AbstractVector, args...) = gröbner_transformation(b, monomialorder(eltype(G)), G, args...)
+    gröbner_basis(b::Backend, G::AbstractVector, args...; kwds...) = gröbner_basis(b, monomialorder(eltype(G)), G, args...; kwds...)
+    gröbner_transformation(b::Backend, G::AbstractVector, args...; kwds...) = gröbner_transformation(b, monomialorder(eltype(G)), G, args...; kwds...)
     """
         basis, transformation = gröbner_transformation(polynomials)
 
@@ -29,7 +29,7 @@ module Gröbner
     This is computed using the Buchberger algorithm with a few standard
     optmizations; see [`PolynomialRings.Gröbner.buchberger`](@ref) for details.
     """
-    gröbner_transformation(G::AbstractVector, args...) = gröbner_transformation(cur_default, G, args...)
+    gröbner_transformation(G::AbstractVector, args...; kwds...) = gröbner_transformation(cur_default, G, args...; kwds...)
     """
         basis = gröbner_basis(polynomials)
 
@@ -38,7 +38,7 @@ module Gröbner
     This is computed using the GWV algorithm; see
     [`PolynomialRings.GröbnerGWV.gwv`](@ref) for details.
     """
-    gröbner_basis(G::AbstractVector, args...) = gröbner_basis(cur_default, G, args...)
+    gröbner_basis(G::AbstractVector, args...; kwds...) = gröbner_basis(cur_default, G, args...; kwds...)
 end
 
 end
