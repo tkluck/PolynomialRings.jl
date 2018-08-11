04c71fdac1baf3254734a4d90631f04ed6736c7f
Timo Kluck <tkluck@infty.nl>
2018-08-11 15:43:58 +0200

    julia 1.0 broadcast/Ref

--- src/EntryPoints.jl
+++ src/EntryPoints.jl
@@ -422,9 +422,9 @@ function getindex(g::NumberedVariableGenerator{Outer,Inner}, i::Integer) where {
     return Outer(construct_monomial(Inner, e))
 end
 
-getindex(g::NumberedVariableGenerator{Outer,Inner}, j::Integer...) where {Outer,Inner} = getindex.(g, j)
+getindex(g::NumberedVariableGenerator{Outer,Inner}, j::Integer...) where {Outer,Inner} = getindex.(Ref(g), j)
 
-getindex(g::NumberedVariableGenerator{Outer,Inner}, ix) where {Outer,Inner} = getindex.(g, ix)
+getindex(g::NumberedVariableGenerator{Outer,Inner}, ix) where {Outer,Inner} = getindex.(Ref(g), ix)
 
 getindex(g::NumberedVariableGenerator) = Channel() do ch
     for i in 1:typemax(Int)
