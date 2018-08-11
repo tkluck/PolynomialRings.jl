ecba8718c623ae19db7d72dca40d1ac7cd8bf806
Timo Kluck <tkluck@infty.nl>
2018-08-11 22:22:37 +0200

    julia 1.0: fixes for echelon()

--- src/PolynomialRings/LinAlgUtil.jl
+++ src/PolynomialRings/LinAlgUtil.jl
@@ -3,7 +3,7 @@ module LinAlgUtil
 if VERSION < v"0.7-"
     import Base: nullspace
 else
-    import LinearAlgebra: nullspace, eye
+    import LinearAlgebra: nullspace, I
 end
 
 abstract type AbstractExactNumber <: Number end
@@ -12,7 +12,9 @@ const ExactNumber = Union{Rational, AbstractExactNumber}
 
 function echelon(M::Matrix{N}) where N <: ExactNumber
     # augment by an identity matrix
-    M_aug = vcat(M, eye(N, size(M,2)))
+    cols = size(M, 2)
+    eye = Matrix{N}(I, cols, cols)
+    M_aug = vcat(M, eye)
     i,j = size(M)
     while i > 0 && j > 0
         # find a pivot in the i'th row left of the j'th column (inclusive)
@@ -48,15 +50,15 @@ end
 function colspan(M::AbstractMatrix{N}) where N <: Number
     M_aug = echelon(M)
 
-    nonzero_cols = [ j for j in indices(M, 2) if any(m != 0 for m in M_aug[indices(M,1),j])]
+    nonzero_cols = [ j for j in 1:size(M, 2) if any(m != 0 for m in M_aug[1:size(M,1),j])]
 
-    return M_aug[indices(M,1), nonzero_cols]
+    return M_aug[1:size(M,1), nonzero_cols]
 end
 
 function nullspace(M::StridedMatrix{N}) where N <: ExactNumber
     M_aug = echelon(M)
 
-    zero_cols = [ j for j in indices(M, 2) if all(m == 0 for m in M_aug[indices(M,1),j])]
+    zero_cols = [ j for j in 1:size(M, 2) if all(m == 0 for m in M_aug[1:size(M,1),j])]
 
     return M_aug[(size(M,1)+1):end, zero_cols]
 end
