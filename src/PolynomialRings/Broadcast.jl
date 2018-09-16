7eab412e1fc81f0e4a5465c5b592834e5199de70
Timo Kluck <tkluck@infty.nl>
2018-09-16 23:25:10 +0200

    broadcast: add docstring; add inplace logic

--- src/PolynomialRings/Broadcast.jl
+++ src/PolynomialRings/Broadcast.jl
@@ -1,3 +1,84 @@
+"""
+Broadcasting for polynomials
+----------------------------
+This module uses Julia's advanced broadcasting support to implement:
+
+1. Term-wise operations, i.e. for polynomials `f` and `g`,
+
+         @. 3f + 4g
+
+   is evaluated term-by-term without allocating intermediate `Polynomial`s for
+   `3f` and `4g`.
+
+2. In-place operations for `BigInt` coefficients, i.e. for `f ∈ @ring ℤ[x]`,
+
+        f .*= 3
+
+   is evaluated by multiplying `f`'s coefficients in-place. More generally, we
+   can re-use the allocated `BigInt` objects in `f` even if the right-hand side
+   operation is more complicated.
+
+The most important combination of these operations is
+
+    @. f = m1*f - m2*t*g
+
+because this is the inner loop of reduction operations (and therefore of Gröbner basis
+computations).
+
+Implementation
+--------------
+
+### Julia broadcasting
+
+Since julia 1.0, broadcasting is implemented by lowering e.g.
+
+    f .= g .* h .+ j
+
+to
+
+    materialize!(f, broadcasted(+, broadcasted(*, g, h)))
+
+In order to define our own behaviour, we override `broadcasted` and `materialize!`.
+
+The function `broadcasted` decides on a `BroadcastStyle` based on its arguments.
+This is typically used to decide the shape of the output, but we re-use this feature
+to:
+- use the `Termwise` style if all arguments are Polynomials or scalars;
+- use the default behaviour otherwise.
+In particular, this means that e.g. `f .* [1,2]` works exactly as you'd
+expect by distributing over the vector elements.
+This is achieved by overriding `BroadcastStyle(...)`.
+
+### Eager evaluation
+
+Some (many) operations do not actually benefit from term-wise operations:
+e.g. we may as well evaluate
+
+     g .* h
+
+as `g * h` if `g` and `h` are polynomials. In this case, the allocation is probably
+negligible compared to the time spent multiplying.
+
+To achieve this, we override the function `broadcasted` with an eager implementation.
+Only those operations that we _know_ to work term-wise efficiently get a more
+specific override.
+
+### Lazy evaluation
+
+For the operations that _do_ work well term-wise, we implement a function
+`iterterms` that takes a `Broadcasted` object or a scalar, and returns
+an object that supports the `iterate` protocol.
+
+The function `iterterms` is paired with a helper function `termsbound` that
+tells the function `materialize!` how many terms to allocate. It is an upper
+bound for how many terms the `iterterms` object may return.
+
+### In-place evaluation
+
+(write something about all the difficulties with potentially aliased coefficients
+between different polynomials and between different terms of the same polynomial(!).
+What's the wisest way to deal with this?)
+"""
 module Broadcast
 
 using Base: RefValue
@@ -16,14 +97,14 @@ broadcastable(p::AbstractMonomial) = Ref(p)
 broadcastable(p::Term) = Ref(p)
 broadcastable(p::Polynomial) = Ref(p)
 
-struct Termwise{Order, P} <: BroadcastStyle end
+struct Termwise{Order, P, inplace} <: BroadcastStyle end
 
-BroadcastStyle(::Type{<:RefValue{P}}) where P<:Polynomial = Termwise{typeof(monomialorder(P)), P}()
+BroadcastStyle(::Type{<:RefValue{P}}) where P<:Polynomial = Termwise{typeof(monomialorder(P)), P, false}()
 BroadcastStyle(s::T, t::T) where T<:Termwise = s
 BroadcastStyle(s::Termwise, t::AbstractArrayStyle{0}) = s
 BroadcastStyle(s::Termwise, t::BroadcastStyle) = t
 
-const BrTermwise{Order,P} = Broadcasted{Termwise{Order,P}}
+const BrTermwise{Order,P,inplace} = Broadcasted{Termwise{Order,P,inplace}}
 const PlusMinus = Union{typeof(+), typeof(-)}
 
 struct InPlace{P}
@@ -79,10 +160,14 @@ termsbound(a::Number) = 1
 termsbound(bc::Broadcasted{<:Termwise, Nothing, <:PlusMinus}) = sum(termsbound, bc.args)
 termsbound(bc::Broadcasted{<:Termwise, Nothing, typeof(*)}) = prod(termsbound, bc.args)
 
+const AbstractInPlace{Order,C} = Union{
+    BrTermwise{Order,<:PolynomialBy{Order,C},true},
+    InPlace{<:PolynomialBy{Order,C}},
+}
 const TermsBy{Order} = Union{
     BrTermwise{Order},
     RefValue{<:PolynomialBy{Order}},
-    InPlace{<:PolynomialBy{Order}},
+    AbstractInPlace{Order},
 }
 # We know how to lazily do +/- if the orders are the same
 broadcasted(st::Termwise{Order}, op::PlusMinus, a::TermsBy{Order}, b::TermsBy{Order}) where Order = Broadcasted{typeof(st)}(op, (a,b))
@@ -127,7 +212,7 @@ const PossiblyBigInt = Union{Int, BigInt}
 mul!(a::BigInt, b::BigInt, c::BigInt) = Base.GMP.MPZ.mul!(a, b, c)
 mul!(a::BigInt, b::BigInt, c::Int) = Base.GMP.MPZ.mul_si!(a, b, c)
 mul!(a::BigInt, b::Int, c::BigInt) = Base.GMP.MPZ.mul_si!(a, c, b)
-function iterterms(op::typeof(*), a::PossiblyBigInt, b::InPlace{<:PolynomialBy{Order,BigInt}}) where Order
+function iterterms(op::typeof(*), a::PossiblyBigInt, b::AbstractInPlace{Order,BigInt}) where Order
     TermsMap(Order(), iterterms(b), true) do t
         if iszero(a)
             return nothing
@@ -139,7 +224,7 @@ function iterterms(op::typeof(*), a::PossiblyBigInt, b::InPlace{<:PolynomialBy{O
         end
     end
 end
-function iterterms(op::typeof(*), a::InPlace{<:PolynomialBy{Order,BigInt}}, b::PossiblyBigInt) where Order
+function iterterms(op::typeof(*), a::AbstractInPlace{Order,BigInt}, b::PossiblyBigInt) where Order
     TermsMap(Order(), iterterms(a), true) do t
         if iszero(b)
             return nothing
@@ -157,9 +242,39 @@ function materialize(bc::BrTermwise{Order,P}) where {Order,P}
     _materialize!(x, bc)
 end
 
+isinplace(::Broadcasted{<:Termwise{<:Any,<:Any,true}}) = true
+isinplace(::Broadcasted{<:Termwise{<:Any,<:Any,false}}) = false
+isinplace(::InPlace) = true
+isinplace(::Any) = false
+
 mark_inplace(y, x) = y
 mark_inplace(bc::RefValue, x) = bc[] === x ? InPlace(bc[]) : bc
-mark_inplace(bc::Broadcasted{St}, x) where St <: Termwise = Broadcasted{St}(bc.f, map(y->mark_inplace(y, x), bc.args))
+mark_inplace(::Type{<:Termwise{Order,P}}) where {Order,P} = Termwise{Order,P,true}
+"""
+Mark at most occurrence of x as in-place in a Broadcasted tree.
+We iterate over the arguments last-to-first and depth-first, so
+we find the _last_ one that will be evaluated _first_. That is
+the place in the optree that we can consider in-place.
+"""
+function mark_inplace(bc::Broadcasted{St}, x) where St <: Termwise
+    st = St
+    args = []
+    n = length(bc.args)
+    while n >= 1
+        a = mark_inplace(bc.args[n], x)
+        pushfirst!(args, a)
+        if isinplace(a)
+            st = mark_inplace(st)
+            break
+        end
+        n -= 1
+    end
+    while n >= 1
+        pushfirst!(args, bc.args[n])
+        n -= 1
+    end
+    Broadcasted{st}(bc.f, tuple(args...))
+end
 
 function materialize!(x::Polynomial, bc::BrTermwise{Order,P}) where {Order,P}
     _materialize!(x, mark_inplace(bc, x))
