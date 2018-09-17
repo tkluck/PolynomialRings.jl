0e95bb5aab8c069a7fbca052cc5922f41e01a124
Timo Kluck <tkluck@infty.nl>
2018-09-17 23:04:52 +0200

    Broadcast: properly bubble up InPlace

--- src/PolynomialRings/Broadcast.jl
+++ src/PolynomialRings/Broadcast.jl
@@ -75,9 +75,29 @@ bound for how many terms the `iterterms` object may return.
 
 ### In-place evaluation
 
-(write something about all the difficulties with potentially aliased coefficients
-between different polynomials and between different terms of the same polynomial(!).
-What's the wisest way to deal with this?)
+For coefficients of type `BigInt`, it can be advantageous to do operations
+in-place, mainly because that saves on allocations.
+
+We can do in-place evaluation in two cases:
+
+ - If one of the source polynomials is also the target, e.g.
+
+       f .= 5 .* g  .- 4 .* f
+
+   (If the target polynomial appears several times, then the _last_ operation to
+   be evaluated can be done in-place.)
+
+ - If we compute an intermediate polynomial, e.g.
+
+       f .* g .+ h
+
+   In this example case, `f .* g` has no term-wise implementation, so we evaluate it
+   eagerly as `f * g`. The resulting polynomial is transient, so we may apply the
+   `... .+ h)` operation in-place.
+
+We represent both cases by wrapping these elements in an `InPlace` struct.
+These structs result in a `TermsMap` with `Inplace=true`, and this property
+bubbles up through the optree.
 """
 module Broadcast
 
@@ -97,14 +117,14 @@ broadcastable(p::AbstractMonomial) = Ref(p)
 broadcastable(p::Term) = Ref(p)
 broadcastable(p::Polynomial) = Ref(p)
 
-struct Termwise{Order, P, inplace} <: BroadcastStyle end
+struct Termwise{Order, P} <: BroadcastStyle end
 
-BroadcastStyle(::Type{<:RefValue{P}}) where P<:Polynomial = Termwise{typeof(monomialorder(P)), P, false}()
-BroadcastStyle(s::T, t::T) where T<:Termwise = s
+BroadcastStyle(::Type{<:RefValue{P}}) where P<:Polynomial = Termwise{typeof(monomialorder(P)), P}()
+BroadcastStyle(s::Termwise{Order, P}, t::Termwise{Order, Q}) where {Order,P,Q} = Termwise{Order, promote_type(P, Q)}
 BroadcastStyle(s::Termwise, t::AbstractArrayStyle{0}) = s
 BroadcastStyle(s::Termwise, t::BroadcastStyle) = t
 
-const BrTermwise{Order,P,inplace} = Broadcasted{Termwise{Order,P,inplace}}
+const BrTermwise{Order,P} = Broadcasted{Termwise{Order,P}}
 const PlusMinus = Union{typeof(+), typeof(-)}
 
 struct InPlace{P}
@@ -112,13 +132,22 @@ struct InPlace{P}
 end
 Base.getindex(x::InPlace) = x.x
 
+broadcastable(x::InPlace) = x
+BroadcastStyle(::Type{<:InPlace{P}}) where P<:Polynomial = Termwise{typeof(monomialorder(P)), P}()
+
 # Eagerly evaluate all expressions involving polynomials, except for the
 # ones we explicitly know how to do. This ensures that e.g. operations
 # with different variables/orders get eagerly evaluated.
 # Note that by the time this function gets called, all the BroadcastStyles
 # have already been bubbled up, so if we still have Termwise as the
 # first argument, we are sure we only have polynomials and scalars.
-broadcasted(::Termwise, op, a, b) = op(a[],b[])
+eager(a) = a
+eager(a::InPlace) = a[]
+eager(a::RefValue) = a[]
+eager(a::Broadcasted) = materialize(a)
+# Note that if we've evaluated eagerly, then we own the resulting Polynomial,
+# so we may apply in-place operations to it.
+broadcasted(::Termwise, op, a, b) = InPlace(op(eager(a), eager(b)))
 
 struct TermsMap{Order,Inplace,Terms,Op}
     terms::Terms
@@ -152,22 +181,18 @@ end
 @inline Base.iterate(t::TermsMap, state::Start) = iterate(t)
 
 termsbound(a::RefValue{<:Polynomial}) = length(terms(a[]))
-iterterms(a::RefValue{<:Polynomial}) = terms(a[])
+iterterms(a::RefValue{<:Polynomial}) = TermsMap(identity, monomialorder(a[]), terms(a[]), false)
 termsbound(a::InPlace{<:Polynomial}) = length(terms(a[]))
-iterterms(a::InPlace{<:Polynomial}) = terms(a[])
+iterterms(a::InPlace{<:Polynomial}) = TermsMap(identity, monomialorder(a[]), terms(a[]), true)
 termsbound(a::RefValue) = 1
 termsbound(a::Number) = 1
 termsbound(bc::Broadcasted{<:Termwise, Nothing, <:PlusMinus}) = sum(termsbound, bc.args)
 termsbound(bc::Broadcasted{<:Termwise, Nothing, typeof(*)}) = prod(termsbound, bc.args)
 
-const AbstractInPlace{Order,C} = Union{
-    BrTermwise{Order,<:PolynomialBy{Order,C},true},
-    InPlace{<:PolynomialBy{Order,C}},
-}
 const TermsBy{Order} = Union{
     BrTermwise{Order},
     RefValue{<:PolynomialBy{Order}},
-    AbstractInPlace{Order},
+    InPlace{<:PolynomialBy{Order}},
 }
 # We know how to lazily do +/- if the orders are the same
 broadcasted(st::Termwise{Order}, op::PlusMinus, a::TermsBy{Order}, b::TermsBy{Order}) where Order = Broadcasted{typeof(st)}(op, (a,b))
@@ -178,15 +203,16 @@ broadcasted(st::Termwise{Order}, op::typeof(*), a::RefValue{<:PolynomialBy{Order
 broadcasted(st::Termwise{Order}, op::typeof(*), a::RefValue{C}, b::RefValue{<:PolynomialOver{C,Order}}) where {Order,C} = Broadcasted{typeof(st)}(op, (a,b))
 broadcasted(st::Termwise{Order}, op::typeof(*), a::Number, b::RefValue{<:PolynomialBy{Order}}) where Order = Broadcasted{typeof(st)}(op, (a,b))
 
-iterterms(bc::Broadcasted{<:Termwise}) = iterterms(bc.f, bc.args...)
+iterterms(x) = x
+iterterms(bc::Broadcasted{<:Termwise}) = iterterms(bc.f, map(iterterms, bc.args)...)
 
-function iterterms(op::PlusMinus, a::TermsBy{Order}, b::TermsBy{Order}) where Order
+function iterterms(op::PlusMinus, a::TermsMap{Order}, b::TermsMap{Order}) where Order
     ≺(a,b) = Base.Order.lt(Order(), a, b)
 
     summands = ParallelIter(
         monomial, coefficient, ≺,
         Zero(), Zero(),
-        iterterms(a), iterterms(b),
+        a, b,
     )
     TermsMap(Order(), summands) do (m, cleft, cright)
         coeff = op(cleft, cright)
@@ -195,14 +221,14 @@ function iterterms(op::PlusMinus, a::TermsBy{Order}, b::TermsBy{Order}) where Or
 end
 
 for T = [Number, RefValue{<:AbstractMonomial}, RefValue{<:Term}]
-    @eval function iterterms(op::typeof(*), a::TermsBy{Order}, b::$T) where Order
-        TermsMap(Order(), iterterms(a)) do t
+    @eval function iterterms(op::typeof(*), a::TermsMap{Order}, b::$T) where Order
+        TermsMap(Order(), a) do t
             iszero(b) ? nothing : t*b
         end
     end
 
-    @eval function iterterms(op::typeof(*), a::$T, b::TermsBy{Order}) where Order
-        TermsMap(Order(), iterterms(b)) do t
+    @eval function iterterms(op::typeof(*), a::$T, b::TermsMap{Order}) where Order
+        TermsMap(Order(), b) do t
             iszero(a) ? nothing : a*t
         end
     end
@@ -212,72 +238,115 @@ const PossiblyBigInt = Union{Int, BigInt}
 mul!(a::BigInt, b::BigInt, c::BigInt) = Base.GMP.MPZ.mul!(a, b, c)
 mul!(a::BigInt, b::BigInt, c::Int) = Base.GMP.MPZ.mul_si!(a, b, c)
 mul!(a::BigInt, b::Int, c::BigInt) = Base.GMP.MPZ.mul_si!(a, c, b)
-function iterterms(op::typeof(*), a::PossiblyBigInt, b::AbstractInPlace{Order,BigInt}) where Order
-    TermsMap(Order(), iterterms(b), true) do t
+# TODO: b should have BigInt coeffs
+function iterterms(op::typeof(*), a::PossiblyBigInt, b::TermsMap{Order,true}) where Order
+    TermsMap(Order(), b, true) do t
         if iszero(a)
             return nothing
         else
-            # TODO: need to ensure that several terms don't share the
-            # same BigInt *in the same polynomial*!!!
+            t = deepcopy(t)
             mul!(t.c, a, t.c)
-            return t
+            t
         end
     end
 end
-function iterterms(op::typeof(*), a::AbstractInPlace{Order,BigInt}, b::PossiblyBigInt) where Order
-    TermsMap(Order(), iterterms(a), true) do t
+# TODO: a should have BigInt coeffs
+function iterterms(op::typeof(*), a::TermsMap{Order,true}, b::PossiblyBigInt) where Order
+    TermsMap(Order(), a, true) do t
         if iszero(b)
             return nothing
         else
-            # TODO: need to ensure that several terms don't share the
-            # same BigInt *in the same polynomial*!!!
+            t = deepcopy(t)
             mul!(t.c, t.c, b)
-            return t
+            t
         end
     end
 end
+inplace!(op, a, b, c) = (a = op(b,c); a)
+inplace!(::typeof(+), a::BigInt, b::BigInt, c::BigInt) = (Base.GMP.MPZ.add!(a,b,c); a)
+inplace!(::typeof(-), a::BigInt, b::BigInt, c::BigInt) = (Base.GMP.MPZ.sub!(a,b,c); a)
+function iterterms(op::PlusMinus, a::TermsMap{Order,true}, b::TermsMap{Order,true}) where Order
+    ≺(a,b) = Base.Order.lt(Order(), a, b)
+
+    summands = ParallelIter(
+        monomial, coefficient, ≺,
+        Zero(), Zero(),
+        a, b,
+    )
+    TermsMap(Order(), summands) do (m, cleft, cright)
+        cleft = inplace!(op, cleft, cleft, cright)
+        iszero(cleft) ? nothing : Term(m, cleft)
+    end
+end
+function iterterms(op::PlusMinus, a::TermsMap{Order,true}, b::TermsMap{Order}) where Order
+    ≺(a,b) = Base.Order.lt(Order(), a, b)
+
+    summands = ParallelIter(
+        monomial, coefficient, ≺,
+        Zero(), Zero(),
+        a, b,
+    )
+    TermsMap(Order(), summands) do (m, cleft, cright)
+        cleft = inplace!(op, cleft, cleft, cright)
+        iszero(cleft) ? nothing : Term(m, cleft)
+    end
+end
+function iterterms(op::PlusMinus, a::TermsMap{Order}, b::TermsMap{Order,true}) where Order
+    ≺(a,b) = Base.Order.lt(Order(), a, b)
+
+    summands = ParallelIter(
+        monomial, coefficient, ≺,
+        Zero(), Zero(),
+        a, b,
+    )
+    TermsMap(Order(), summands) do (m, cleft, cright)
+        cright = inplace!(op, cright, cleft, cright)
+        iszero(cright) ? nothing : Term(m, cright)
+    end
+end
+
+materialize(bc::InPlace) = bc[]
 
 function materialize(bc::BrTermwise{Order,P}) where {Order,P}
     x = deepcopy(zero(P))
     _materialize!(x, bc)
 end
 
-isinplace(::Broadcasted{<:Termwise{<:Any,<:Any,true}}) = true
-isinplace(::Broadcasted{<:Termwise{<:Any,<:Any,false}}) = false
-isinplace(::InPlace) = true
-isinplace(::Any) = false
-
-mark_inplace(y, x) = y
-mark_inplace(bc::RefValue, x) = bc[] === x ? InPlace(bc[]) : bc
-mark_inplace(::Type{<:Termwise{Order,P}}) where {Order,P} = Termwise{Order,P,true}
+mark_inplace(y, x) = (y, :notfound)
+mark_inplace(bc::RefValue, x) = bc[] === x ? (InPlace(bc[]), :found) : (bc, :notfound)
 """
-Mark at most occurrence of x as in-place in a Broadcasted tree.
-We iterate over the arguments last-to-first and depth-first, so
-we find the _last_ one that will be evaluated _first_. That is
-the place in the optree that we can consider in-place.
+Mark at most a single occurrence of x as in-place in a Broadcasted tree. We
+iterate over the arguments last-to-first and depth-first, so we find the _last_
+one that will be evaluated _first_. That is the place in the optree that we can
+consider in-place.
 """
 function mark_inplace(bc::Broadcasted{St}, x) where St <: Termwise
-    st = St
     args = []
-    n = length(bc.args)
-    while n >= 1
-        a = mark_inplace(bc.args[n], x)
-        pushfirst!(args, a)
-        if isinplace(a)
-            st = mark_inplace(st)
-            break
+    found = :notfound
+    for n = length(bc.args):-1:1
+        if found == :notfound
+            (a, found) = mark_inplace(bc.args[n], x)
+            pushfirst!(args, a)
+        else
+            pushfirst!(args, bc.args[n])
         end
-        n -= 1
     end
-    while n >= 1
-        pushfirst!(args, bc.args[n])
-        n -= 1
-    end
-    Broadcasted{st}(bc.f, tuple(args...))
+    return (Broadcasted{St}(bc.f, tuple(args...)), found)
 end
 
 function materialize!(x::Polynomial, bc::BrTermwise{Order,P}) where {Order,P}
-    _materialize!(x, mark_inplace(bc, x))
+    (bc′, found) = mark_inplace(bc, x)
+    if found == :found
+        tgt = deepcopy(zero(x))
+        _materialize!(tgt, bc′)
+        resize!(x.terms, length(tgt.terms))
+        copyto!(x.terms, tgt.terms)
+    elseif found == :notfound
+        _materialize!(x, bc′)
+    else
+        @assert "unreachable"
+    end
+    x
 end
 
 function _materialize!(x::Polynomial, bc::BrTermwise{Order,P}) where {Order,P}
