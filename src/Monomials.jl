55edd6718b7e6288eca8a5846045625e330b2f4a
Timo Kluck <tkluck@infty.nl>
2020-02-07 14:53:01 +0100

    Ensure at-monomial returns an AbstractMonomial
    This is intended to guard against something like
    
        @monomial(c[1] * x)
    
    working and returning
    
        @monomial(c[1]) * @monomial(x)
    
    which is a polynomial.

--- src/Monomials.jl
+++ src/Monomials.jl
@@ -2,6 +2,7 @@ module Monomials
 
 import SparseArrays: SparseVector, sparsevec
 
+import ..AbstractMonomials: AbstractMonomial
 import ..NamingSchemes: NamingScheme, Named, Numbered, namingscheme, num_variables
 import ..MonomialOrderings: MonomialOrder
 
@@ -78,14 +79,14 @@ function recurse_monomial_from_expr(expr::Expr)
         return :(exp(monomialtype($(QuoteNode(x)), Inf), sparsevec([$i], [1])))
     elseif expr.head == :call
         args = map(recurse_monomial_from_expr, expr.args[2:end])
-        return Expr(expr.head, expr.args[1], args...)
+        return Expr(expr.head, esc(expr.args[1]), args...)
     else
         return Expr(expr.head, map(recurse_monomial_from_expr, expr.args)...)
     end
 end
 
 macro monomial(expr)
-    return recurse_monomial_from_expr(expr)
+    return Expr(:(::), recurse_monomial_from_expr(expr), :AbstractMonomial)
 end
 
 end
