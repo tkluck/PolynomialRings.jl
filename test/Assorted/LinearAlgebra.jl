08025366d0c1a140a9ffb4b9e1e5404a1d807467
Sheehan Olver <solver@mac.com>
2023-04-18 11:26:47 +0100

    Add test with mat*vec

--- test/Assorted/LinearAlgebra.jl
+++ test/Assorted/LinearAlgebra.jl
@@ -7,4 +7,5 @@ using LinearAlgebra
     R, (x, y) = polynomial_ring(:x, :y, basering=Int)
 
     @test [x,y]' * [-y,x] == transpose([x,y]) * [-y,x] == dot([x,y], [-y,x]) == 0
+    @test rem.([x y; -y x] * [x,y], 1-x^2-y^2) == [1,0]
 end
